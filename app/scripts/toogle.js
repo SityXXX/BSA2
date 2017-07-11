df29d4fbe742844fec3e86e650fd69b9d26fb7dc
Yura Bazaliy <yurchykxxx@gmail.com>
2017-07-11 15:30:30 +0300

    Footer fixes

--- app/scripts/toogle.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('#menu-btn').onclick = function() {
-  document.querySelector('#menu-list').style.left = 0;
-};
-document.querySelector('#main-menu-close').onclick = function() {
-  document.querySelector('#menu-list').style.left = '-100%';
-};
